ac4539cc7a146ec7fb9834358fd55e84cb7c7175
goyyapazham <nalanda99@gmail.com>
2017-02-05 14:36:32 -0500

    it works

--- db_builder.py
+++ db_builder.py
@@ -1,8 +1,27 @@
 from pymongo import MongoClient
+import csv
 
-server = MongoClient("149.89.161.100")
+server = MongoClient("127.0.0.1")
 db = server.TeamMango
 c = db.students
 
-courses = open("courses.csv").read().strip().split("\n")[1:]
-peeps = open("peeps.csv").read().strip().split("\n")[1:]
+f = open("peeps.csv", "r")
+pdata = csv.DictReader(f)
+
+g = open("courses.csv", "r")
+cdata = csv.DictReader(g)
+
+for peep in pdata:
+    d = {}
+    d['name'] = peep['name']
+    d['age'] = int(peep['age'])
+    d['id'] = int(peep['id'])
+    for course in cdata:
+        if course['id'] == peep['id']:
+            d[course['code']] = int(course['mark'])
+    g.seek(0)
+    #print d
+    c.insert_one(d)
+
+f.close()
+g.close()
